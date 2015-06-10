1d10c0a272857e01a9bef6d462d4636483a867fe
Satjiv Kanwar <xsaedren@yahoo.com>
2015-06-09 21:29:19 -0400

    Added all number inputs

--- Entro.py
+++ Entro.py
@@ -1,3 +1,14 @@
 class Cell:
     def __init__ (self,a):
         self.ss = a
+
+def setup():
+    lol = []
+    for a in range(9):
+        y = raw_input ("Put in the numbers BITCH!: ")
+        pseudoephedrine = y.split()
+        hagrid = map (int, pseudoephedrine)
+        aquababe = map (Cell, hagrid)
+        print aquababe
+        lol.append (aquababe)
+    return lol
